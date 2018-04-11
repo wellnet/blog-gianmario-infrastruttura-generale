61b76d3c3df52f13f62f8b8ecb292e546472b523
gianmario-mereu <gmereu@wellnet.it>
2018-04-11 18:35:51 +0200

    GA-1 init - codice del progetto

--- RoboFile.php
+++ RoboFile.php
@@ -20,10 +20,9 @@ class RoboFile extends \Robo\Tasks {
   /**
    * Constant variable Project.
    */
-  const PROJECT_HCMS = 'hcms';
-  const PROJECT_CRM = 'crm';
-  const PROJECT_AO = 'ao';
-  const PROJECT_ST = 'st';
+  const PROJECT_PR1 = 'pr1';
+  const PROJECT_PR2 = 'pr2';
+  const PROJECT_PR3 = 'pr3';
 
   /**
    * Options arguments for command line.
@@ -94,8 +93,8 @@ class RoboFile extends \Robo\Tasks {
     $this->stopOnFail(TRUE);
   }
 
-  public function initHIS($opts = self::OPTS) {
-    $this->say("Init HIS project");
+  public function initAG($opts = self::OPTS) {
+    $this->say("Init AG project");
 
     $env = $opts['environment'];
     $site = $opts['site'];
@@ -104,21 +103,19 @@ class RoboFile extends \Robo\Tasks {
       throw new \Exception("L'ambiente " . $env . " non esiste.");
     }
 
-    // solr volume permission
-    $this->init(self::PROJECT_AO, $env, $site);
-    $this->setupSolrVolumePermission(self::PROJECT_AO);
-    $this->init(self::PROJECT_ST, $env, $site);
-    $this->setupSolrVolumePermission(self::PROJECT_ST);
+    // Call methods to init AG project.
+    $this->init(self::PROJECT_PR1, $env, $site);
+    $this->setupSolrVolumePermission(self::PROJECT_PR1);
   }
 
   /**
-   * Build HCMS project.
+   * Build PR1 project.
    *
    * @param array $opts
    * @return void
    */
-  public function buildHcms($opts = self::OPTS) {
-    $this->say("Build HCMS");
+  public function buildPr1($opts = self::OPTS) {
+    $this->say("Build PR1");
     
     $env = $opts['environment'];
     $site = $opts['site'];
@@ -127,25 +124,24 @@ class RoboFile extends \Robo\Tasks {
       throw new \Exception("L'ambiente " . $env . " non esiste.");
     }
 
-    $this->init(self::PROJECT_HCMS, $env, $site);
-    $this->backupDBProject(self::PROJECT_HCMS);
-    $this->importDBProject(self::PROJECT_HCMS);
+    $this->init(self::PROJECT_PR1, $env, $site);
+    $this->backupDBProject(self::PROJECT_PR1);
+    $this->importDBProject(self::PROJECT_PR1);
 
     // Custom setup for HCMS
     // 1° Setup elasticsearch
-    $this->setupElasticSearchHCMS();
-    $this->setupDrupalAccount(self::PROJECT_HCMS);
-    $this->setupDrupalFileTemporayPath(self::PROJECT_HCMS);
+    $this->setupDrupalAccount(self::PROJECT_PR1);
+    $this->setupDrupalFileTemporayPath(self::PROJECT_PR1);
   }
 
   /**
-   * Build CRM project.
+   * Build PR2 project.
    *
    * @param array $opts
    * @return void
    */
-  public function buildCrm($opts = self::OPTS) {
-    $this->say("Build Crm");
+  public function buildPr2($opts = self::OPTS) {
+    $this->say("Build PR2");
     
     $env = $opts['environment'];
     $site = $opts['site'];
@@ -154,23 +150,23 @@ class RoboFile extends \Robo\Tasks {
       throw new \Exception("L'ambiente " . $env . " non esiste.");
     }
 
-    $this->init(self::PROJECT_CRM, $env, $site);
-    $this->backupDBProject(self::PROJECT_CRM);
-    $this->importDBProject(self::PROJECT_CRM);
+    $this->init(self::PROJECT_PR2, $env, $site);
+    $this->backupDBProject(self::PROJECT_PR2);
+    $this->importDBProject(self::PROJECT_PR2);
 
-    // Custom setup for CRM
-    $this->setupDrupalAccount(self::PROJECT_CRM);
-    $this->setupDrupalFileTemporayPath(self::PROJECT_HCMS);
+    // Custom setup for PR2
+    $this->setupDrupalAccount(self::PROJECT_PR2);
+    $this->setupDrupalFileTemporayPath(self::PROJECT_PR2);
   }
 
   /**
-   * Build Area Operatori project.
+   * Build PR3  project.
    *
    * @param array $opts
    * @return void
    */
-  public function buildAO($opts = self::OPTS) {
-    $this->say("Build Area Operatori");
+  public function buildPr3($opts = self::OPTS) {
+    $this->say("Build PR3");
     
     $env = $opts['environment'];
     $site = $opts['site'];
@@ -179,59 +175,13 @@ class RoboFile extends \Robo\Tasks {
       throw new \Exception("L'ambiente " . $env . " non esiste.");
     }
 
-    $this->init(self::PROJECT_AO, $env, $site);
-    $this->backupDBProject(self::PROJECT_AO);
-    $this->importDBProject(self::PROJECT_AO);
+    $this->init(self::PROJECT_PR3, $env, $site);
+    $this->backupDBProject(self::PROJECT_PR3);
+    $this->importDBProject(self::PROJECT_PR3);
 
     // Custom setup for Area Operatori
-    $this->setupDrupalAccount(self::PROJECT_AO);
-    $this->setupDrupalFileTemporayPath(self::PROJECT_HCMS);
-    $this->setupCustomAO();
-  }
-
-  /**
-   * Build Sardegna Turismo project.
-   *
-   * @param array $opts
-   * @return void
-   */
-  public function buildST($opts = self::OPTS) {
-    $this->say("Build Sardegna Turismo");
-
-    $env = $opts['environment'];
-    $site = $opts['site'];
-
-    if(!in_array($env, $this->envs)) {
-      throw new \Exception("L'ambiente " . $env . " non esiste.");
-    }
-
-    $this->init(self::PROJECT_ST, $env, $site);
-    $this->backupDBProject(self::PROJECT_ST);
-    $this->importDBProject(self::PROJECT_ST);
-
-    // Custom setup for Sardegna Turismo
-    $this->setupDrupalAccount(self::PROJECT_ST);
-    $this->setupDrupalFileTemporayPath(self::PROJECT_ST);
-    $this->setupCustomST();
-  }
-
-  /**
-   * Configure hcms to point to local elasticsearch instance.
-   */
-  private function setupElasticSearchHCMS() {
-    $this->say('Point HCMS to local ElastichSearch.');
-    $database = Robo::Config()->get(self::PROJECT_HCMS . '.databases.drupal');
-    $user = $database['user'];
-    $password = $database['password'];
-    $host = $database['host'];
-    $port = $database['port'];
-    $dbname = $database['dbname'];
-
-    $sql = "UPDATE his_elasticsearch_connector_cluster set url = \"http://elasticsearch:9200\" WHERE cluster_id = \"elasticcluster\";";
-
-    $command_sql = "mysql -u " . $user . " -p" . $password . 
-      " -P " . $port . " -h " . $host . " " . $dbname .  " -e '".$sql."'";
-    $this->taskExec($command_sql)->run();
+    $this->setupDrupalAccount(self::PROJECT_PR3);
+    $this->setupDrupalFileTemporayPath(self::PROJECT_PR3);
   }
 
   /**
@@ -246,75 +196,12 @@ class RoboFile extends \Robo\Tasks {
   }
 
   /**
-   * Custom setup for ST.
-   *
-   * @return void
-   */
-  private function setupCustomST() {
-    // local solr configuration
-    $this->setupLocalSolrServerForST();
-  }
-
-  /**
-   * Create a local solr server for ST and associate to it all indexes.
-   */
-  private function setupLocalSolrServerForST() {
-    $drush_container = Robo::Config()->get('st.drush_container');
-    $command_bash = "scripts/SardegnaTurismoCreateLocalSolrServer.drush";
-    $command = "docker exec -it " . $drush_container . " sh -c '" . $command_bash . "'";
-
-    $this->say("Set local solr settings for project Sardegna Turismo");
-    $this->taskExec($command)->run();
-  }
-
-  /**
-   * Copy accreditation role file in private directory.
-   *
-   * @return void
-   */
-  private function copyAccreditationRoleFile() {
-    $this->say("Copy accreditation role file in private directory");
-
-    $to = $this->getBasePath() . "/" . 
-      $this->getProjectRootDirectory(self::PROJECT_AO) . "/" . 
-      $this->getProjectCodeDirectory(self::PROJECT_AO) . "/" .
-      Robo::Config()->get(self::PROJECT_AO . '.drupal.private') . "/" . 
-      Robo::Config()->get(self::PROJECT_AO . '.drupal.accreditation_filename');
-    $setting = self::PROJECT_AO . "-settings";
-    $from = $this->getBasePath() . "/" . self::FOLDER_DATA . "/" . $setting . "/" . Robo::Config()->get(self::PROJECT_AO . '.drupal.accreditation_filename');
-
-    $this->taskFilesystemStack()->copy($from, $to)->run();
-  }
-
-  /**
-   * Set up drupal variables for extra_utils.
-   *
-   * @return void
-   */
-  private function setExtraUtilsApiRestAnagrafe() {
-    $variables = [
-      'extra_utils_apirestanagrafe',
-      'extra_utils_apirestanagrafe_nuovoidras',
-      'extra_utils_apirestanagrafe_new',
-      'extra_utils_apirestanagrafe_edit',
-      'extra_utils_apirestanagrafe_newdocuments',
-    ];
-    $drush_container = Robo::Config()->get(self::PROJECT_AO . '.drush_container');
-
-    foreach($variables as $variable) {
-      $command_drush = "drush vset " . $variable . " 'http://127.0.0.1'";
-      $command = "docker exec -it " . $drush_container . " " . $command_drush;
-      $this->taskExec($command)->run();
-    }
-  }
-
-  /**
-   * Execute databases backup for a hyperlocal project
+   * Execute databases backup for a AG project
    *
    * @param string $project
-   *   hyperlocal project id.
+   *   AG project id.
    */
-  private function backupDBProject($project = self::PROJECT_HCMS) {
+  private function backupDBProject($project = self::PROJECT_PR1) {
     
     $folder = $this->getBasePath() . "/" . self::FOLDER_DATABASE_BACKUPS;
     $this->say('Backup database for ' . $project . '.');
@@ -344,12 +231,12 @@ class RoboFile extends \Robo\Tasks {
   }
 
   /**
-   * Execute databases import for a hyperlocal project
+   * Execute databases import for a AG project
    *
    * @param string $project
-   *   hyperlocal project id.
+   *   AG project id.
    */
-  private function importDBProject($project = self::PROJECT_HCMS) {
+  private function importDBProject($project = self::PROJECT_PR1) {
 
     $folder = $this->getBasePath() . "/" . self::FOLDER_DATABASE_DUMPS;
     $this->say('Import databases for ' . $project . '.');
@@ -379,12 +266,12 @@ class RoboFile extends \Robo\Tasks {
   }
 
   /**
-   * Setup ras-admin password for a hyperlocal project.
+   * Setup admin password for a AG project.
    *
    * @param string $project
-   *   hyperlocal project id.
+   *   AG project id.
    */
-  private function setupDrupalAccount($project = self::PROJECT_HCMS) {
+  private function setupDrupalAccount($project = self::PROJECT_PR1) {
     $drush_container = Robo::Config()->get($project . '.drush_container');
     $account = Robo::Config()->get($project . '.account');
     $username = $account['username'];
@@ -402,7 +289,7 @@ class RoboFile extends \Robo\Tasks {
    * @param string $project
    *   hyperlocal project id.
    */
-  private function setupDrupalFileTemporayPath($project = self::PROJECT_HCMS) {
+  private function setupDrupalFileTemporayPath($project = self::PROJECT_PR1) {
     $drush_container = Robo::Config()->get($project . '.drush_container');
     $file_temporary_path = Robo::Config()->get($project . '.drupal.file_temporary_path');
     $command_drush = "drush vset file_temporary_path " . $file_temporary_path;
@@ -418,22 +305,22 @@ class RoboFile extends \Robo\Tasks {
    * @param $project
    *   hyperlocal project id.
    */
-  private function setupSolrVolumePermission($project = self::PROJECT_ST) {
-    $drush_container = Robo::Config()->get($project . '.drush_container');
+  private function setupSolrVolumePermission($project = self::PROJECT_PR1) {
+    $solr_container = Robo::Config()->get($project . '.solr_container');
     $command_bash = "chown 1000.1000 /opt/solr/example/solr/data/ -R";
-    $command = "docker exec -it " . $drush_container . " sh -c '" . $command_bash . "'";
+    $command = "docker exec -it -u root " . $solr_container . " sh -c '" . $command_bash . "'";
 
     $this->say("Set solr volume ownership for project: " . $project);
     $this->taskExec($command)->run();
   }
 
   /**
-   * Undocumented function
+   * Create a privat drupal directory.
    *
    * @param [type] $project
    * @return void
    */
-  private function createDrupalPrivateDirectory($project = self::PROJECT_HCMS) {
+  private function createDrupalPrivateDirectory($project = self::PROJECT_PR1) {
     $base_path = $this->getBasePath() . "/";
     $project_directory = $this->getProjectRootDirectory($project) . "/";
     $code_directory = $this->getProjectCodeDirectory($project) . "/";
@@ -568,84 +455,4 @@ class RoboFile extends \Robo\Tasks {
     return $this->getBasePath() . "/" . Robo::Config()->get('site_configuration.site_root');
   }
 
-  /**
-   * Retrieve a DrushStack.
-   *
-   * @return \Boedah\Robo\Task\Drush\DrushStack
-   *   Retrieve an object DrushStack with the root folder set.
-   */
-  private function getDrush() {
-    /** @var \Boedah\Robo\Task\Drush\DrushStack $drush_stack */
-    $drush_stack = $this->task(\Boedah\Robo\Task\Drush\DrushStack::class, Robo::Config()->get('drush_path'));
-    $drush_stack->drupalRootDirectory($this->getBasePath() . "/" . Robo::Config()->get('site_configuration.root'));
-    return $drush_stack;
-  }
-
-  /**
-   * Setup file and directory for installation.
-   *
-   * Include:
-   *  - clear file and directory site;
-   *  - init file and directory site.
-   */
-  private function setupInstallation() {
-    $this->clearFilesystem();
-    $this->initFilesystem();
-  }
-
-  /**
-   * Clears the directory structure for site.
-   */
-  private function clearFilesystem() {
-    $this->say('Clears the directory structure for site');
-    $base_path = $this->getSiteRoot() . "/sites/" . Robo::Config()->get('site_configuration.sub_dir');
-    $this->taskFilesystemStack()
-      ->chmod($base_path, 0775, 0000, TRUE)
-      ->chmod($base_path, 0775)
-      ->remove($base_path . '/files')
-      ->remove($base_path . '/settings.php')
-      ->remove($base_path . '/services.yml')
-      ->run();
-  }
-
-    /**
-   * Creates the directory structure for site.
-   */
-  private function initFilesystem() {
-    $this->say('Creates the directory structure for site');
-    $base_path = $this->getSiteRoot() . "/sites/" . Robo::Config()->get('site_configuration.sub_dir');
-    $this->taskFilesystemStack()
-      ->chmod($base_path, 0775, 0000, TRUE)
-      ->mkdir($base_path . '/files')
-      ->mkdir($base_path . '/files/logs')
-      ->chmod($base_path . '/files', 0775, 0000, TRUE)
-      ->copy($base_path . '/default.settings.php', $base_path . '/settings.php')
-      ->copy($base_path . '/default.services.yml', $base_path . '/services.yml')
-      ->run();
-  }
-
-  /**
-   * Install Drupal.
-   *
-   * @param array $properties_custom
-   *   Properties custom. es. $properties['site_configuration']['profile'].
-   */
-  private function install($properties_custom = []) {
-    $this->say('Install Drupal');
-    $properties = $this->properties;
-    // User replace and not merge because merge duplicate entry with same key.
-    // @see http://php.net/manual/en/function.array-replace-recursive.php
-    $properties = array_replace_recursive($properties, $properties_custom);
-    $this->getDrush()
-      ->siteName(Robo::Config()->get('site_configuration.name'))
-      ->siteMail(Robo::Config()->get('site_configuration.mail'))
-      ->accountMail(Robo::Config()->get('account.mail'))
-      ->accountName(Robo::Config()->get('account.name'))
-      ->accountPass(Robo::Config()->get('account.pass'))
-      ->dbUrl(Robo::Config()->get('databases.default.url'))
-      ->locale(Robo::Config()->get('site_configuration.locale'))
-      ->sitesSubdir(Robo::Config()->get('site_configuration.sub_dir'))
-      ->siteInstall(Robo::Config()->get('site_configuration.profile'))
-      ->run();
-  }
 }
